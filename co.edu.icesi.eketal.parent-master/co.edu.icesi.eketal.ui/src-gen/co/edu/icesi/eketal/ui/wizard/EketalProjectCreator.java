d23ce83c8da2b5cb27f87af1d90b2fe1f3214ff9
1107082145 <1107082145@SC305C07.academico.icesi.edu.co>
2016-07-01 09:36:28 -0500

    Fixed generated problems
    There will be problems if the modified classes are the ones in the
    src-gen source folder because when the mw2e re-generate the artifacts,
    all of the edited classes will reset. So the thing is to create a class
    to extend the class of the src-gen package and edit, then override the
    configure bind to point the created class

--- co.edu.icesi.eketal.parent-master/co.edu.icesi.eketal.ui/src-gen/co/edu/icesi/eketal/ui/wizard/EketalProjectCreator.java
+++ co.edu.icesi.eketal.parent-master/co.edu.icesi.eketal.ui/src-gen/co/edu/icesi/eketal/ui/wizard/EketalProjectCreator.java
@@ -42,34 +42,8 @@ public class EketalProjectCreator extends AbstractPluginProjectCreator {
 	@Override
 	protected PluginProjectFactory createProjectFactory() {
 		PluginProjectFactory projectFactory = super.createProjectFactory();
-		projectFactory.addImportedPackages(eketalRequiredPackages());
-		projectFactory.addRequiredBundles(eketalRequiredBundles());
-		projectFactory.addProjectNatures(aspectjNature());
 		projectFactory.setWithPluginXml(false);
 		return projectFactory;
-	}
-
-	private String aspectjNature() {
-		return "org.eclipse.ajdt.ui.ajnature";
-	}
-	
-	private String mavenNature() {
-		return "org.eclipse.m2e.core.maven2Nature";
-	}
-
-	private List<String> eketalRequiredBundles() {
-		ArrayList<String> array = new ArrayList<>();
-		array.add("co.edu.icesi.eketal");
-		array.add("co.edu.icesi.eketal.lib.osgi;bundle-version=\"1.0.0\"");
-		array.add("org.aspectj.runtime");
-		return array;
-	}
-
-	private List<String> eketalRequiredPackages() {
-		ArrayList<String> array = new ArrayList<>();
-		array.add("co.edu.icesi.ketal.distribution");
-		array.add("co.edu.icesi.ketal.distribution.transports.jgroups");
-		return array;
 	}
 
 	@Override
