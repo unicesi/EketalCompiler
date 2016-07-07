112709b53735a9ec3b0028883a6052f26515edaf
camilo <camilo@localhost>
2016-07-07 13:12:16 -0500

    Worked in the project creator, also edited the required aspectj bundle
    -Added some java and aspectj packages
    -Added in the feature the require of aspectj

--- co.edu.icesi.eketal.parent-master/co.edu.icesi.eketal.ui/src/co/edu/icesi/eketal/ui/wizard/JavaProjectCreator.java
+++ co.edu.icesi.eketal.parent-master/co.edu.icesi.eketal.ui/src/co/edu/icesi/eketal/ui/wizard/JavaProjectCreator.java
@@ -2,6 +2,8 @@ package co.edu.icesi.eketal.ui.wizard;
 
 import org.eclipse.xtext.generator.IOutputConfigurationProvider;
 import org.eclipse.xtext.ui.XtextProjectHelper;
+import org.aspectj.ajdt.core.AspectJCore;
+import org.eclipse.jdt.core.JavaCore;
 
 import com.google.inject.Inject;
 
@@ -16,9 +18,16 @@ public class JavaProjectCreator extends EketalProjectCreator {
 	}
 	
 	@Override
+	protected String[] getBuilders() {
+		return new String[]{
+			JavaCore.BUILDER_ID, XtextProjectHelper.BUILDER_ID, AspectJCore.BUILDER_ID
+		};
+	};
+	
+	@Override
 	protected String[] getProjectNatures() {
 		return new String[] {
-			XtextProjectHelper.NATURE_ID, "org.eclipse.ajdt.ui.ajnature"
+			JavaCore.NATURE_ID, XtextProjectHelper.NATURE_ID, AspectJCore.NATURE_ID
 		};
 	}
 	
