8b85ec8d3b24e894a1cd5923576ec57f54aa9e81
1107082145 <1107082145@sc208e16.academico.icesi.edu.co>
2016-07-15 10:45:59 -0500

    Added the contents of the classes
    -The test of the automaton
    -The content of the .eketal file

--- Example-project/src/test/java/test/TestAutomaton.java
+++ Example-project/src/test/java/test/TestAutomaton.java
@@ -4,11 +4,26 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import co.edu.icesi.eketal.automaton.MiPrimerAuromata;
+import co.edu.icesi.ketal.core.Automaton;
+import co.edu.icesi.ketal.core.Event;
+import co.edu.icesi.ketal.core.NamedEvent;
+
 public class TestAutomaton {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	Automaton instance = MiPrimerAuromata.getInstance();
+
+    @Test
+    public void testCase(){
+        Event eventHello = new NamedEvent("eventHello");
+        Event eventWorld = new NamedEvent("eventWorld");
+
+
+        instance.getCurrentState().toString();
+        assertTrue(instance.evaluate(eventHello));
+        instance.getCurrentState().toString();
+        assertTrue(instance.evaluate(eventWorld));
+        instance.getCurrentState().toString();
+    }
 
 }
