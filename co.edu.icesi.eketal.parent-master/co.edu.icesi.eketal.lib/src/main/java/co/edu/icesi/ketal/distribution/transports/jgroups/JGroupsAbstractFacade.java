35c08ff591f0b5a96bd41fded1c4d55894e152a3
Camilo Pimienta Rosero <cfpimienta@icesi.edu.co>
2017-11-15 11:56:19 -0500

    Updated the configurations of TCP and UDP
    This change has configured TCP with only TCP protocols, and UDP, this
    configuration has been successful in deploys

--- co.edu.icesi.eketal.parent-master/co.edu.icesi.eketal.lib/src/main/java/co/edu/icesi/ketal/distribution/transports/jgroups/JGroupsAbstractFacade.java
+++ co.edu.icesi.eketal.parent-master/co.edu.icesi.eketal.lib/src/main/java/co/edu/icesi/ketal/distribution/transports/jgroups/JGroupsAbstractFacade.java
@@ -40,26 +40,123 @@ public abstract class JGroupsAbstractFacade extends ReceiverAdapter {
 	RequestOptions opts;
 	protected URL address;
 	// RequestHandler disp;
+	
+	/*
+	
+	private String props ="TCP(bind_port=7800;"+
+         "recv_buf_size=${tcp.recv_buf_size:5M};"+
+         "send_buf_size=${tcp.send_buf_size:5M};"+
+         "max_bundle_size=64K;"+
+         "max_bundle_timeout=30;"+
+         "use_send_queues=true;"+
+         "sock_conn_timeout=300;"+
 
+         "timer_type=new3;"+
+         "timer.min_threads=4;"+
+         "timer.max_threads=10;"+
+         "timer.keep_alive_time=3000;"+
+         "timer.queue_max_size=500;"+
+         
+         "thread_pool.enabled=true;"+
+         "thread_pool.min_threads=2;"+
+         "thread_pool.max_threads=8;"+
+         "thread_pool.keep_alive_time=5000;"+
+         "thread_pool.queue_enabled=true;"+
+         "thread_pool.queue_max_size=10000;"+
+         "thread_pool.rejection_policy=discard;"+
+
+         "oob_thread_pool.enabled=true;"+
+         "oob_thread_pool.min_threads=1;"+
+         "oob_thread_pool.max_threads=8;"+
+         "oob_thread_pool.keep_alive_time=5000;"+
+         "oob_thread_pool.queue_enabled=false;"+
+         "oob_thread_pool.queue_max_size=100;"+
+         "oob_thread_pool.rejection_policy=discard):"+
+                         
+    	"TCPPING(async_discovery=true;"+
+         "initial_hosts="${jgroups.tcpping.initial_hosts:supervisor1[7800],supervisor2[7800],supervisor4[7800],zookeeper[7800],supervisor6[7800]};"+
+         "port_range=2):"+
+
+	   "MERGE3(min_interval=10000;max_interval=30000):"+	   
+	   "FD_SOCK():" +
+	   "FD(timeout=12000;max_tries=3):"+
+	   "VERIFY_SUSPECT(timeout=2000):" +
+		"BARRIER():" +                           
+	   "pbcast.NAKACK2(use_mcast_xmit=false;discard_delivered_msgs=true):" +
+       "UNICAST3():"+
+
+	   "pbcast.STABLE(stability_delay=1000;desired_avg_gossip=50000;max_bytes=4M):" +
+
+	   "pbcast.GMS(join_timeout=2000;print_local_addr=true;view_bundling=true):"+
+	   //"pbcast.GMS(join_timeout=5000;print_local_addr=true;view_bundling=true):Causal(causal_order_prot_interest=false)";
+	   "MFC(max_credits=2M;min_threshold=0.4):"+
+
+	   "FRAG2(frag_size=60k):" +
+	   "pbcast.STATE_TRANSFER()";
+	 */
+	
+	
 	// The properties configuring the Jgroups communication stack
 	// Properties are managed by class DistributionProperties
 	//String props = DistributionProperties.getInstance()
 			//.getStack("AspectsGroup");
-	  private String props = "UDP(mcast_send_buf_size=212K;mcast_port=45566;ucast_recv_buf_size=212K;" +
-			   "mcast_addr=228.8.8.8;mcast_recv_buf_size=212K;max_bundle_size=60000;" +
-			   "max_bundle_timeout=30;" +
-			   "ucast_send_buf_size=212K;ip_ttl=32):" +
-			   "PING(timeout=2000;num_initial_members=3):" +
-			   //Added to simulate lost messages
-			   "DISCARD(up=0.05;excludeItself=true):" +
-			   "MERGE2(min_interval=10000;max_interval=20000):" +
-			   "FD(timeout=2000;max_tries=4):" +
-			   "VERIFY_SUSPECT(timeout=1500):" +
-			   "pbcast.NAKACK(use_mcast_xmit=false;retransmit_timeout=600,1200,2400,4800):" +
-			   "pbcast.STABLE(stability_delay=1000;desired_avg_gossip=20000;max_bytes=0):" +
-			   "FRAG(frag_size=8192):" +
-			   //"pbcast.GMS(join_timeout=5000;print_local_addr=true;view_bundling=true):Causal(causal_order_prot_interest=false)";
-			   "pbcast.GMS(join_timeout=5000;print_local_addr=true;view_bundling=true)";
+	private String props = "UDP(mcast_port=${jgroups.udp.mcast_port:45588};"+
+			"tos=8;"+
+			"ucast_recv_buf_size=210K;"+
+			"ucast_send_buf_size=210K;"+
+		    "mcast_recv_buf_size=210K;"+
+		    "mcast_send_buf_size=210K;"+
+		    "max_bundle_size=64K;"+
+		    "max_bundle_timeout=30;"+
+		    "enable_diagnostics=true;"+
+		    "thread_naming_pattern=cl;"+
+		    "logical_addr_cache_max_size=1000;"+
+
+		    "timer_type=new3;"+
+		    "timer.min_threads=2;"+
+		    "timer.max_threads=4;"+
+		    "timer.keep_alive_time=3000;"+
+		    "timer.queue_max_size=500;"+
+			"timer.rejection_policy=abort;"+
+
+		    "thread_pool.enabled=true;"+
+		    "thread_pool.min_threads=10;"+
+		    "thread_pool.max_threads=80;"+
+		    "thread_pool.keep_alive_time=5000;"+
+		    "thread_pool.queue_enabled=true;"+
+		    "thread_pool.queue_max_size=50000;"+
+		    "thread_pool.rejection_policy=discard;"+
+
+		    "oob_thread_pool.enabled=true;"+
+		    "oob_thread_pool.min_threads=5;"+
+		    "oob_thread_pool.max_threads=80;"+
+		    "oob_thread_pool.keep_alive_time=5000;"+
+		    "oob_thread_pool.rejection_policy=discard):"+
+
+			"PING(break_on_coord_rsp=true):" +
+			"MERGE3(min_interval=10000;max_interval=30000):" +
+
+			"FD_SOCK():" +
+			"FD_ALL(timeout=12000):" +
+			"VERIFY_SUSPECT(timeout=2000):" +
+			"BARRIER():" +
+
+		  	"pbcast.NAKACK2(use_mcast_xmit=true;xmit_interval=500;xmit_table_num_rows=100;xmit_table_msgs_per_row=2000;"+
+			"xmit_table_max_compaction_time=35000;max_msg_batch_size=500;discard_delivered_msgs=true):" +
+					   
+		    "UNICAST3(xmit_interval=500;xmit_table_num_rows=100;xmit_table_msgs_per_row=2000;"+
+		    "xmit_table_max_compaction_time=60000;max_msg_batch_size=500):"+
+
+			"pbcast.STABLE(stability_delay=2000;desired_avg_gossip=50000;max_bytes=4M):" +
+		    "pbcast.GMS(join_timeout=10000;print_local_addr=true;view_bundling=true,merge_timeout=7000,max_bundling_time=1000,resume_task_timeout=15000):"+
+
+			"UFC(max_credits=4M;min_threshold=0.4):"+
+			"MFC(max_credits=4M;min_threshold=0.4):"+
+
+			"FRAG2(frag_size=60k):" +
+		    "RSVP(resend_interval=2000;timeout=10000):"+
+			//"pbcast.GMS(join_timeout=5000;print_local_addr=true;view_bundling=true):Causal(causal_order_prot_interest=false)";
+			"pbcast.STATE_TRANSFER()";
 
 	String groupName;
 	EventBroker jeb;
